3fd1a3dd0f1e6c1e5a4df70db96db3ba3cb4e5e0
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-04-23 22:11:15 +0300

    Imperative Hook

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Timer from './Timer'
+import Implerative from './Implerative';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Timer />
+    <Implerative />
   </React.StrictMode>
 );
 
